c052599c062445728379fdcdf4916f1bf79ca65d
pawanv048 <pawanv048@gmail.com>
2022-05-13 19:06:33 +0530

    data passing

--- src/Screens/Profile/Profile.js
+++ src/Screens/Profile/Profile.js
@@ -1,15 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, {Component} from 'react'
 
-const Profile = ({ route, navigation }) => {
-    //console.log("profile properties==>", props)
-    //const { navigation, route } = props
-    //console.log("show props", route.params)
-    //const { title } = route.params.title
-    console.log("routes =>" ,route)
+const Profile = ({route, navigation}) => {
+   
+    const { learning } = route.params
+    console.log("props =>" , learning)
+    
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            {/* <Text>{title}</Text> */}
+            <Text>{learning}</Text>
             <Text>profile</Text>
         </View>
     )
